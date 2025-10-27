83a819886dc233f1b9fc876461fb073b161ce17e
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-27 09:23:57 +0700

    Menambahkan sinkronisasi database Sequelize sebelum menjalankan server dan menampilkan log error

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const db = require('./models');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Database sync error:', err);
+    });
