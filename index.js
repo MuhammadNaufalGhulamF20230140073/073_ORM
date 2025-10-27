a248fb841909f2239c3112715f4796824672b8ac
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-27 09:23:16 +0700

    Menambahkan middleware untuk memproses data body request dalam format JSON dan form

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-cons express = require('express');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const db = require('./models');
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
