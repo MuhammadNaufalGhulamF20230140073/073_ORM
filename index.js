64b225a55ab29eaa021827b42620bd9ba43d398f
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-27 09:58:25 +0700

    tambah endpoint GET /komik untuk mengambil semua data komik

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ app.post('/komik', async(req, res) => {
         res.send(err);
     }
 });
+
+app.get('/komik', async(req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (error) {
+        res.send(Err);
+    }
+});
